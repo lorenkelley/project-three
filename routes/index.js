49c5477a0ff292d6cd5451142532d5337e726411
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-11-13 20:22:28 -0500

    setting up application

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+// router.get('', .index)
+// router.get('', .show)
+// router.delete('', .delete)
+// router.patch('', .update)
+// router.post('' , .create)
+
+module.exports = router
