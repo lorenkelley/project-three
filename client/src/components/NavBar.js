91e7a8f4b79551cee107e36669db0e9cbfe59900
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-11-15 12:27:54 -0500

    creating NavBar.js component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,13 +3,23 @@ import {Link} from 'react-router-dom'
 // importing Link to have links on nav bar react does not use a tags
 import styled from 'styled-components'
 
+const NavBarStyles = styled.div `
+
+
+
+
+
+`
+
 class NavBar extends Component {
     render() {
         return (
+            <NavBarStyles>
             <div>
                 <Link to='/'>Home</Link>
                 <Link to=''>Sign In</Link>
             </div>
+            </NavBarStyles>
         );
     }
 }
