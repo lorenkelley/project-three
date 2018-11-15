b8421a80495dc3e1de642b72f4760d8b6f7cbb43
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-11-15 14:47:22 -0500

    properties controller updated

--- controllers/propertiesController.js
+++ controllers/propertiesController.js
@@ -2,7 +2,13 @@ const Location = require('../models/Location')
 const Properties = require('../models/Properties')
 
 const propertiesController = {
-     index: (req,res)
+     index: (req,res) => {
+         var locationId = req.params.locationId 
+         Location.findById(locationId).populate('properties')
+         .then((location) =>{
+             res.send(location.properties)
+         })
+     }
 }
 
 
