4b7b427238be2e390102881fe2f2ad4f0d9d0505
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-11-15 16:15:14 -0500

    removing react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
+import HomePage from './components/HomePage';
 
 class App extends Component {
   render () {
     return (
       <Router>
         <div>
-          <NavBar/>
+        
           <Switch>
-            {/* <Route exact path="/" component={}/>
-            // you can name yourroutes anything but when you use compnents and call axios
-            // the routesyou have in your  index.js you link them to the components 
-            <Route path="/:id" component={}/> */}
+              
+            {/*  you can name your routes on the App.js anything (they are simply linking the components) 
+             when you use compnents and call axios
+             the routes you have in your  index.js you link them to the components to those specfic routes 
+             <Route path="/:id" component={}/>  */}
+             <Route path="/" component={HomePage}/> 
           </Switch>
         </div>
       </Router>
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar'
+
 
 class HomePage extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Homepage</h1>
+                  <NavBar/>
             </div>
         );
     }
